18ad0b0024095a2256b850e8dfea07fa49230652
박찬진 <68864422+developerChans@users.noreply.github.com>
2025-01-08 14:10:38 +0900

    Revert "팀당 최대 인원 수 설정"

--- js/getRandomTeam.js
+++ js/getRandomTeam.js
@@ -1,12 +1,4 @@
-const splitter = (str, separator) => {
-    let memberList = [];
-    if(separator === ' ') {
-        memberList = str.split(/\s+/);
-    } else {
-        memberList = str.split(separator);
-    }
-    return memberList;
-}
+const splitter = (str, separator) => str.split(separator);
 
 const generateRandomKey = () => Math.random();
 
@@ -34,6 +26,7 @@ const getRandomNum = (limit, teamMemberNumber) => {
 
 const getRandomTeam = ({ members, teamMemberNumber, separator }) => {
     const memberList = splitter(members, separator);
+
     if (memberList.length > 40) {
         alert('최대 40명까지만 입력 가능합니다.');
         return [];
